545efa8c6b573f0129fb2731d9c3aeacb4dab741
vcmirko <mirko@vancolen.com>
2024-11-16 17:57:48 +0100

    setup

--- deploy_lod.sh
+++ deploy_lod.sh
@@ -2,18 +2,6 @@ set enforce 0
 
 dnf install -y git vim podman-compose
 
-mkdir /srv/apps
-cd /srv/apps
-
-git init
-git clone https://github.com/ansibleguy76/ansibleforms-docker.git
-cd ansibleforms-docker/data/playbooks
-wget https://123websites.be/maf.tar 
-tar -xf maf.tar
-rm -f maf.tar
-
-cd ../../
-
 # write access will be needed on the datafolder
 chmod -R 664 ./data
 # the mysql init folder needs execute rights 
