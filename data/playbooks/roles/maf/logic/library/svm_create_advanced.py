3b17cec4df43ad2ebf3dc109b93ddae89af2e4c2
vcmirko <mirko@vancolen.com>
2025-04-14 18:29:24 +0200

    voldr

--- data/playbooks/roles/maf/logic/library/svm_create_advanced.py
+++ data/playbooks/roles/maf/logic/library/svm_create_advanced.py
@@ -69,26 +69,13 @@ def run_module():
      # apply logic
     try:
 
+        # process source first
+
         # template
-        log("Set templates")
+        log("Set template")
         source["template"] = f"{service}_{service_level}"
-        if dr_type == "svm_dr":
-            destination["template"] = "svm_dr"
 
-
-
-        # create cluster peer object
-        cluster_peer = []
         source_cluster = source["cluster"]
-
-        log("Set intercluster ips")
-        # in lod, we can calculate the intercluster ips, based on the management ip
-        # it's a bit of a hack, but it works.  We could have also looked them up by rest api
-        source_cluster["intercluster_ips"] = source["cluster"]["management_ip"].replace(".101", ".121").replace(".102", ".123")
-        cluster_peer.append(source_cluster)
-        destination_cluster = destination.get("cluster", {})
-        destination_cluster["intercluster_ips"] = destination.get("cluster", {}).get("management_ip", "").replace(".101", ".121").replace(".102", ".123")
-        cluster_peer.append(destination_cluster)
 
         log("Set lifs")
         # complete svm lifs
@@ -96,78 +83,20 @@ def run_module():
         if source_svm.get("lifs", None):   
             source_svm["lifs"][0]["node"] = f"{source_cluster['name']}-01" # set node name
 
-        log("Set junction paths if volumes are given")
-        # complete volumes
-        source_volumes = source.get("volumes", [])
-        # loop over volumes
-        for volume in source_volumes:
-            volume["junction_path"] = f"/{volume['name']}"
-
-        log("Set destination svm")
-        # set destination svm
-        destination_svm = {
-            "name" : f"{source_svm['name']}_dr"
-        }
-        destination["svm"] = destination_svm    
-
-        log("Set vserver peer")
-        # vserver peer
-        vserver_peer = []
-        peer_source_svm = {
-            "cluster": {
-                "name": source_cluster["name"],
-                "management_ip": source_cluster["management_ip"]
-            },
-            "svm": {
-                "name": source_svm["name"]
-            }
-        }
-        peer_destination_svm = {
-            "cluster": {
-                "name": destination_cluster.get("name", ""),
-                "management_ip": destination_cluster.get("management_ip", "")
-            },
-            "svm": {
-                "name": destination_svm["name"]
-            }
-        }
-        vserver_peer.append(peer_source_svm)
-        vserver_peer.append(peer_destination_svm)
-
-        log("Set snapmirror")
-        # set snapmirror
-        snapmirror = {
-            "source": {
-                "svm": {
-                    "name": source_svm["name"]
-                },
-                "cluster": {
-                    "name": source_cluster["name"],
-                    "management_ip": source_cluster["management_ip"]
-                },
-            },
-            "destination": {
-                "svm": {
-                    "name": destination_svm["name"]
-                },
-                "cluster": {
-                    "name": destination_cluster.get("name", ""),
-                    "management_ip": destination_cluster.get("management_ip", "")
-                },
-            },
-            "identity_preserve": "full"
-        }
-
-        log("Set cifs server")
         # set cifs if required
         if service == "smb":
+
+            log("Set cifs server")            
             source["cifs"] = {}
             source["cifs"]["name"] = source_svm["name"].replace("_smb_","").replace("_","")
 
-        if service == "nfs":
+        if service == "nfs" or service == "vmw":
             # if nfs, add export policy
+            log("Set export policy")
             source["export_policy"] = {}
             source["export_policy"]["name"] = f"xp_default"
+
+            # apply the default export policy to the root volume
             source["svm"]["root_volume"] = {}
             source["svm"]["root_volume"]["name"] = f"{source_svm['name']}_root"
             source["svm"]["root_volume"]["export_policy"] = source["export_policy"]["name"]
@@ -181,17 +110,139 @@ def run_module():
             source["export_policy"]["rules"] = []
             source["export_policy"]["rules"].append(rule)
 
-        log("Assign everything to the vars_external dict")
         # set clusters (for delete)
-        clusters = []
-        clusters.append(source_cluster)
-        clusters.append(destination_cluster)
+        # clusters = []
+        # clusters.append(source_cluster)
 
-        source["clusters"] = clusters
-        destination["clusters"] = clusters
-        ve["snapmirror"] = snapmirror
-        ve["vserver_peer"] = vserver_peer
-        ve["cluster_peer"] = cluster_peer     
+        # check dr
+        if is_dr:
+            log("Dr is enabled")
+
+            # create cluster peer object
+            cluster_peer = []
+            log("Set intercluster ips")
+            # in lod, we can calculate the intercluster ips, based on the management ip
+            # it's a bit of a hack, but it works.  We could have also looked them up by rest api
+            source_cluster["intercluster_ips"] = source["cluster"]["management_ip"].replace(".101", ".121").replace(".102", ".123")
+            cluster_peer.append(source_cluster)
+            destination_cluster = destination.get("cluster", {})
+            destination_cluster["intercluster_ips"] = destination.get("cluster", {}).get("management_ip", "").replace(".101", ".121").replace(".102", ".123")
+            cluster_peer.append(destination_cluster)
+
+            # assign to vars_external
+            ve["cluster_peer"] = cluster_peer
+
+            log("Set destination svm")
+            # set destination svm
+            destination_svm = {
+                "name" : f"{source_svm['name']}_dr"
+            }
+            destination["svm"] = destination_svm    
+
+            log("Set vserver peer")
+            # vserver peer
+            vserver_peer = []
+            peer_source_svm = {
+                "cluster": {
+                    "name": source_cluster["name"],
+                    "management_ip": source_cluster["management_ip"]
+                },
+                "svm": {
+                    "name": source_svm["name"]
+                }
+            }
+            peer_destination_svm = {
+                "cluster": {
+                    "name": destination_cluster.get("name", ""),
+                    "management_ip": destination_cluster.get("management_ip", "")
+                },
+                "svm": {
+                    "name": destination_svm["name"]
+                }
+            }
+            vserver_peer.append(peer_source_svm)
+            vserver_peer.append(peer_destination_svm)
+
+            # assign to vars_external
+            ve["vserver_peer"] = vserver_peer
+
+            # clusters.append(destination_cluster)            
+
+            if dr_type == "volume_dr":
+                log("Volume dr is enabled")
+
+
+                log("Set lifs")
+                destination_svm["lifs"] = source_svm["lifs"].copy() # copy same lifs from source
+                # complete svm lifs
+                if destination_svm.get("lifs", None):   
+                    destination_svm["lifs"][0]["node"] = f"{destination_cluster['name']}-01" # set node name
+
+                # set cifs if required
+                if service == "smb":
+
+                    log("Set cifs server")            
+                    destination["cifs"] = {}
+                    destination["cifs"]["name"] = destination_svm["name"].replace("_smb_","").replace("_","")
+
+                if service == "nfs" or service == "vmw":
+
+                    # if nfs, add export policy
+                    log("Set export policy")
+                    destination["export_policy"] = {}
+                    destination["export_policy"]["name"] = f"xp_default"
+
+                    # apply the default export policy to the root volume
+                    destination["svm"]["root_volume"] = {}
+                    destination["svm"]["root_volume"]["name"] = f"{destination_svm['name']}_root"
+                    destination["svm"]["root_volume"]["export_policy"] = destination["export_policy"]["name"]
+                
+                    # auto add 0.0.0.0/0 to export policy for readonly
+                    rule = {}
+                    rule["client_match"] = "0.0.0.0/0"
+                    rule["ro_rule"] = "sys"
+                    rule["rw_rule"] = "never"
+                    rule["super_user_security"] = "none"
+                    destination["export_policy"]["rules"] = []
+                    destination["export_policy"]["rules"].append(rule)
+
+            else:
+                log("SVM dr is enabled")
+                # apply template
+                destination["template"] = "svm_dr"
+
+                log("Set snapmirror")
+                # set snapmirror
+                snapmirror = {
+                    "source": {
+                        "svm": {
+                            "name": source_svm["name"]
+                        },
+                        "cluster": {
+                            "name": source_cluster["name"],
+                            "management_ip": source_cluster["management_ip"]
+                        },
+                    },
+                    "destination": {
+                        "svm": {
+                            "name": destination_svm["name"]
+                        },
+                        "cluster": {
+                            "name": destination_cluster.get("name", ""),
+                            "management_ip": destination_cluster.get("management_ip", "")
+                        },
+                    },
+                    "identity_preserve": "full"
+                }		
+
+                ve["snapmirror"] = snapmirror
+
+
+
+        # source["clusters"] = clusters
+        # destination["clusters"] = clusters
+        
+ 
 
         log("Drop dr related stuff if not is_dr")
         # now we check if is_dr is set, if not, we remove all dr-related stuff
@@ -201,12 +252,6 @@ def run_module():
             ve.pop("vserver_peer", None)
             ve.pop("cluster_peer", None)
             ve.pop("destination", None)
-
-        # if no dr or volume dr, remove destination too
-        if is_dr and dr_type == "volume_dr":
-            ve.pop("snapmirror", None)
-            ve.pop("vserver_peer", None)
-            ve.pop("cluster_peer", None)
 
     except Exception as e:
         log(repr(e))
